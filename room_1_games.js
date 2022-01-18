e7d301cd6cd5652de4f08df347150295a8b97fa9
mador till <mador.till@gmail.com>
2022-01-18 10:50:02 +0200

    r1p10

--- room_1_games.js
+++ room_1_games.js
@@ -8,31 +8,6 @@ pop_sign_click = () => {
     }); 
 }
 
-restart_1 = () => {
-    // r1p3
-
-
-    // r1p7
-    counter_r1p7_signs_order = 0
-    $("#r1p7 .board").addClass("empty");
-    // new signs order
-    for (let i = 0; i < arr_r1p7_signs_order.length ; i++) {
-        let random = Math.floor(Math.random() * arr_r1p7_signs_order.length) + 1;
-        while (arr_r1p7_signs_order.includes(random)) {
-            random = Math.floor(Math.random() * arr_r1p7_signs_order.length) + 1;
-        }
-        arr_r1p7_signs_order[i] = random;
-    }
-    console.log(arr_r1p7_signs_order);
-    // signs return to original location
-    $("#r1p7 .sign").css({top: "38vw", left: "42.5vw"});
-    // signs dissappear accept from the first
-    for (let i = 2; i <= arr_r1p7_signs_order.length ; i++) {
-        switch_class($(`#r1p7 .drag.data-num-${i}`), "none", "block");
-    }
-
-}
-
 // the parameter is the clicked correct sign
 r1p3_clicked_correct = (item) => {
     item.animate({opacity: `0`}, 200, function() {
@@ -82,6 +57,44 @@ r1p7_dropped_correct = (drag, drop) => {
 
 var counter_r1p10_folder = 0;
 var counter_r1p10_trash = 0;
+var arr_r1p10_files_order = [
+    {
+        top: "14vw",
+        left: "81vw",
+        transform: "rotate(2deg)",
+        used: false
+    },
+    {
+        top: "16vw",
+        left: "70vw",
+        transform: "rotate(-3deg)",
+        used: false
+    },
+    {
+        top: "12vw",
+        left: "57vw",
+        transform: "rotate(1deg)",
+        used: false
+    },
+    {
+        top: "31vw",
+        left: "77vw",
+        transform: "rotate(8deg)",
+        used: false
+    },
+    {
+        top: "33vw",
+        left: "60vw",
+        transform: "rotate(5deg)",
+        used: false
+    },
+    {
+        top: "27vw",
+        left: "48vw",
+        transform: "rotate(-6deg)",
+        used: false
+    }
+]
 r1p10_dropped_correct = (drag, drop) => {
     // disable item dragging
     drag.draggable("option", "disabled", true);
@@ -100,4 +113,48 @@ r1p10_dropped_correct = (drag, drop) => {
     if ((counter_r1p10_folder === $(`#${matrix[nRoom][nPage].divName} .drag-1`).length) && ((counter_r1p10_trash === $(`#${matrix[nRoom][nPage].divName} .drag-2`).length))) {
         V_X(true);
     } 
+}
+
+
+restart_1 = () => {
+    // r1p3
+    switch_class($("#r1p3 .item"), "hidden", "visible");
+
+    // r1p7
+    counter_r1p7_signs_order = 0
+    $("#r1p7 .board").addClass("empty");
+    // new signs order
+    for (let i = 0; i < arr_r1p7_signs_order.length ; i++) {
+        let random = Math.floor(Math.random() * arr_r1p7_signs_order.length) + 1;
+        while (arr_r1p7_signs_order.includes(random)) {
+            random = Math.floor(Math.random() * arr_r1p7_signs_order.length) + 1;
+        }
+        arr_r1p7_signs_order[i] = random;
+    }
+    console.log(arr_r1p7_signs_order);
+    // signs return to original location
+    $("#r1p7 .sign").css({top: "38vw", left: "42.5vw"});
+    // signs dissappear accept from the first
+    for (let i = 2; i <= arr_r1p7_signs_order.length ; i++) {
+        switch_class($(`#r1p7 .drag.data-num-${i}`), "none", "block");
+    }
+
+    // r1p10
+    counter_r1p10_folder = 0;
+    counter_r1p10_trash = 0;
+    $("#r1p10 .drag-2").css({width: "10.5vw"});
+    $("#r1p10 .data-num-2.drag-2").attr("src", `assets/media/exer3/exer3_bikurofe.svg`);
+    $("#r1p10 .data-num-2.drag-6").attr("src", `assets/media/exer3/exer3_kabala.svg`);
+    // new files order
+    for (let i = 0; i < arr_r1p10_files_order.length ; i++) {
+        arr_r1p10_files_order[i].used = false;
+    }
+    for (let i = 0; i < arr_r1p10_files_order.length ; i++) {
+        let random = Math.floor(Math.random() * arr_r1p10_files_order) + 1;
+        while (arr_r1p10_files_order[random].used) {
+            random = Math.floor(Math.random() * arr_r1p10_files_order) + 1;
+        }
+        (`#r1p10 .file.data-num-${i}`).css({top: arr_r1p10_files_order[random].top, left: arr_r1p10_files_order[random].left, transform: arr_r1p10_files_order[random].transform});
+        arr_r1p10_files_order[random].used = true;
+    }
 }
