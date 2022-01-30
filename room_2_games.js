b245044092fa8bd1840edbee8cd081d240bd8102
mador till <mador.till@gmail.com>
2022-01-30 18:37:29 +0200

    slider

--- room_2_games.js
+++ room_2_games.js
@@ -1,10 +1,21 @@
 // activates the slider
 // works only on items with class "slider"
 // r2p2
-pop_slider = () => {
+pop_r2p2_slider = () => {
     $(`#${matrix[nRoom][nPage].divName} .slider`).slider({
         min: 8,
         max: 17,
-        value: 12.5
+        value: 13,
+        step: 1,
+        slide: function(event,ui) {
+            console.log(ui.value);
+        }
+    });
+    $(`#${matrix[nRoom][nPage].divName}`).keyup(function(event) {
+        // if the user clicked enter
+        if (event.which === 13) {
+            alert('Enter is pressed!');
+        }
+        $(`#${matrix[nRoom][nPage].divName}`).off("keyup");
     });
 }
